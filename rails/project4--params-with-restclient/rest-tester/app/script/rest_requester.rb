503dd1ca2729dee9cda9e595465188d6dfc5883f
schepens83 <schepens@gmail.com>
2015-11-08 11:42:48 +0100

    add rails project 4

---
+++ rails/project4--params-with-restclient/rest-tester/app/script/rest_requester.rb
@@ -0,0 +1,16 @@
+require 'rest-client'
+
+url = "http://localhost:3000/users/"
+puts RestClient.get(url)
+
+url = "http://localhost:3000/users/new"
+puts RestClient.get(url)
+
+url = "http://localhost:3000/users/1/edit"
+puts RestClient.get(url)
+
+url = "http://localhost:3000/users/1"
+puts RestClient.get(url)
+
+url = "http://localhost:3000/users"
+puts RestClient.post(url, "")
