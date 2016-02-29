38499edfd9a86dcad6842775b462ad2d855cc54a
schepens83 <schepens@gmail.com>
2016-02-29 18:37:43 +0100

    proj9-adds seed data for new events with timing in future and past

--- rails/project9--private-events/private-events/db/seeds.rb
+++ rails/project9--private-events/private-events/db/seeds.rb
@@ -9,3 +9,9 @@ u1 = User.create(name: "owner 1", age: 10)
 e1 = Event.create(description: "event 1", location: 'holland', date: Time.now, owner: u1)
 u2 = e1.invitees.create(name: "part. 1", age: 11)
 u3 = e1.invitees.create(name: "part. 2", age: 12)
+
+u2 = User.create(name: "Fiene", age: 1)
+20.times do |i|
+	Event.create(description: "event future #{i}", location: "holland district #{i}", date: Time.now + i, owner: u2)
+	Event.create(description: "event past #{i}", location: "holland district #{i}", date: Time.now - i, owner: u2)
+end
