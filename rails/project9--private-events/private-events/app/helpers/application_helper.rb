fe8565e96952717c5f8618e1a3fb60aa752f9184
schepens83 <schepens@gmail.com>
2016-02-29 11:45:26 +0100

    proj9-start from scratch

--- rails/project9--private-events/private-events/app/helpers/application_helper.rb
+++ rails/project9--private-events/private-events/app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-	def current_user
-		user_name = cookies[:name]
-		if user_name
-			user_name
-		else
-			nil
-		end
-	end
 end
--- rails/project9--private-events/private-events/app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ActiveRecord::Base
-	has_many :event_owners, :class_name => "Event", :foreign_key => "owner_id"
-end
