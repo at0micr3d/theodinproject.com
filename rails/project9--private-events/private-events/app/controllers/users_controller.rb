78d72f198554272623b1363ac2a3c7bc095cbdf8
schepens83 <schepens@gmail.com>
2016-02-29 12:26:36 +0100

    proj9-adds User model and controller + views

---
+++ rails/project9--private-events/private-events/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+	before_action :set_user, only: [:show]
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user, notice: 'User was successfully created.'
+		else
+			render action: 'new', notice: 'User creation was not successfull.'
+		end
+	end
+
+	def show
+	end
+
+	private
+	# Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+	def user_params
+      params.require(:user).permit(:name, :age)
+    end
+end
--- rails/project9--private-events/private-events/db/seeds.rb
+++ rails/project9--private-events/private-events/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u1 = User.create(name: "owner 1", age: 10)
