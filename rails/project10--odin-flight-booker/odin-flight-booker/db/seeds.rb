19a89f1cfe644cec22211753cf0f0c0a1eb31767
schepens83 <schepens@gmail.com>
2016-03-06 15:47:48 +0100

    proj10-set up relationships between flight and airports and seeds database with flights and airports

--- rails/project10--odin-flight-booker/odin-flight-booker/db/seeds.rb
+++ rails/project10--odin-flight-booker/odin-flight-booker/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create some airports
+a1 = Airport.create(code: "SFO", city: "San Fransisco")
+a2 = Airport.create(code: "AMS", city: "Amsterdam")
+a3 = Airport.create(code: "EIN", city: "Eindhoven")
+a4 = Airport.create(code: "CDG", city: "Paris")
+
+
+# create some flights between airports
+f1 = Flight.create(number: 1, from_airport_id: 1, to_airport_id: 2, :departure_time => (DateTime.now + 3.days), :flight_duration => (3600 * 10))
+f2 = Flight.create(number: 2, from_airport_id: 2, to_airport_id: 1, :departure_time => (DateTime.now + 2.days), :flight_duration => (3600 * 10))
+f3 = Flight.create(number: 3, from_airport_id: 2, to_airport_id: 3, :departure_time => (DateTime.now + 10.days), :flight_duration => (3600 * 2))
+f4 = Flight.create(number: 4, from_airport_id: 3, to_airport_id: 4, :departure_time => (DateTime.now + 10.days), :flight_duration => (3600 * 2))
