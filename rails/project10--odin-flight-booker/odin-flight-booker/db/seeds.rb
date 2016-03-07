5f9e78c80ad37b14c58cc05c524627a4775110cb
schepens83 <schepens@gmail.com>
2016-03-07 13:48:36 +0100

    proj10-changes seed data for flights

--- rails/project10--odin-flight-booker/odin-flight-booker/db/seeds.rb
+++ rails/project10--odin-flight-booker/odin-flight-booker/db/seeds.rb
@@ -7,7 +7,18 @@ a4 = Airport.create(code: "CDG", city: "Paris")
 
 
 # create some flights between airports
-f1 = Flight.create(number: 1, from_airport_id: 1, to_airport_id: 2, :departure_time => (DateTime.now + 3.days), :flight_duration => (3600 * 10))
-f2 = Flight.create(number: 2, from_airport_id: 2, to_airport_id: 1, :departure_time => (DateTime.now + 2.days), :flight_duration => (3600 * 10))
-f3 = Flight.create(number: 3, from_airport_id: 2, to_airport_id: 3, :departure_time => (DateTime.now + 10.days), :flight_duration => (3600 * 2))
-f4 = Flight.create(number: 4, from_airport_id: 3, to_airport_id: 4, :departure_time => (DateTime.now + 10.days), :flight_duration => (3600 * 2))
+(1..20).each do |i|
+	Flight.create(number: 1, from_airport_id: 1, to_airport_id: 2, :departure_time => (DateTime.now + i.days), :flight_duration => (3600 * 10))
+end
+
+(1..20).each do |i|
+	Flight.create(number: 2, from_airport_id: 2, to_airport_id: 1, :departure_time => (DateTime.now + i.days), :flight_duration => (3600 * 10))
+end
+
+(1..20).each do |i|
+	Flight.create(number: 3, from_airport_id: 2, to_airport_id: 3, :departure_time => (DateTime.now + i.days + 10.days), :flight_duration => (3600 * 2))
+end
+
+(1..20).each do |i|
+	Flight.create(number: 4, from_airport_id: 3, to_airport_id: 4, :departure_time => (DateTime.now + i.days + 10.days), :flight_duration => (3600 * 2))
+end
