f8a74e2fcb8a011f923e8458f2d3baf6581db2db
schepens83 <schepens@gmail.com>
2016-03-07 15:29:55 +0100

    proj10-adds search form with multiple options

--- rails/project10--odin-flight-booker/odin-flight-booker/app/controllers/flights_controller.rb
+++ rails/project10--odin-flight-booker/odin-flight-booker/app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
 	def index
+		@airport_options = Airport.all.map { |a| [a.code, a.id] }
+		@flight_date_options = Flight.all.map { |f| [f.departure_time.to_date, f.id] }
 		unless params[:q] == nil
 			@flights = Flight.all
 		end
