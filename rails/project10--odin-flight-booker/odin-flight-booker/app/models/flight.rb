bbea06efc8e4e3a45cbc95e5fc9ace3f69f149c5
schepens83 <schepens@gmail.com>
2016-03-11 16:04:44 +0100

    proj10-adds filter in flights#index

--- rails/project10--odin-flight-booker/odin-flight-booker/app/models/flight.rb
+++ rails/project10--odin-flight-booker/odin-flight-booker/app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ActiveRecord::Base
 	belongs_to :from_airport, class_name: "Airport", foreign_key: "airport_id"
   belongs_to :to_airport, class_name: "Airport", foreign_key: "airport_id"
+
+  scope :from_airport, ->(a) { where("from_airport_id = ?", a) }
+  scope :to_airport, ->(a) { where("to_airport_id = ?", a) }
+  scope :flight_date, ->(d) { where("date(departure_time) in (?)", d) }
 end
