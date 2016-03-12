21a1a10c5ce0313a73fbde992133e1d21b26539c
schepens83 <schepens@gmail.com>
2016-03-12 10:33:24 +0100

    proj10-adds booking create and show controller actions and views

--- rails/project10--odin-flight-booker/odin-flight-booker/app/controllers/bookings_controller.rb
+++ rails/project10--odin-flight-booker/odin-flight-booker/app/controllers/bookings_controller.rb
@@ -2,16 +2,36 @@ class BookingsController < ApplicationController
 	def new
 		@flight = Flight.find(params[:flight_id])
 		@booking = Booking.new
-		params[:nr_passengers].to_i.times do
+		nr_passengers = params[:nr_passengers].to_i
+		nr_passengers.times do
 			@booking.customers << Customer.new
 		end
 	end
 
+	def show
+		@booking = Booking.find(params[:id])
+		@passengers = @booking.customers
+		@flight = @booking.flight
+	end
+
 	def create
-		
+		@booking = Booking.new(booking_params)
+		@booking.date = DateTime.now
+		@booking.price = @booking.customers.count * 500
+
+		if @booking.save
+			flash[:succes] = "You have booked the flight"
+			redirect_to @booking
+		else
+			flash[:error] = "Could not book your flight"
+			render 'flights#index'
+		end
 	end
 
 	private 
 
+	def booking_params
+		params.require(:booking).permit(:flight_id, customers_attributes: [ :name, :age, :passport_nr, :street, :city ])
+	end
 
 end
--- rails/project10--odin-flight-booker/odin-flight-booker/app/models/booking.rb
+++ rails/project10--odin-flight-booker/odin-flight-booker/app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ActiveRecord::Base
 	has_many :customers
 	accepts_nested_attributes_for :customers
 	has_one :flight
+
+	validates_associated :customers
 end
