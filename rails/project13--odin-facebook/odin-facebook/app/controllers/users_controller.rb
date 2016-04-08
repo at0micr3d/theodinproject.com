59da169c4e5a96777d491808ca82b4249e9ac4c5
schepens83 <schepens@gmail.com>
2016-04-08 17:31:04 +0200

    proj13--adds profile info incl Gravatar to profile

--- rails/project13--odin-facebook/odin-facebook/app/controllers/users_controller.rb
+++ rails/project13--odin-facebook/odin-facebook/app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-    	if params[:id].is_a?(Integer)
+    	if params[:id]
     		@user = User.find(params[:id]) 
     	else
     		@user = User.find(current_user.id)
--- rails/project13--odin-facebook/odin-facebook/app/helpers/users_helper.rb
+++ rails/project13--odin-facebook/odin-facebook/app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+	# Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase.strip)
+    gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}.jpg"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", size: "50")
+  end
 end
